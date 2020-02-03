dd7e64580e4ee1dd1e8361ca51be836afa26d7b9
vs17673704 <30440336+vs17673704@users.noreply.github.com>
2020-02-03 10:05:02 +0530

    Update Answers.java

--- question-query-portal/src/main/java/com/portal/question/model/Answers.java
+++ question-query-portal/src/main/java/com/portal/question/model/Answers.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "answers")
+@Table(name = "answers",, unique = true, nullable=false)
 public class Answers 
 {
 	@Id
